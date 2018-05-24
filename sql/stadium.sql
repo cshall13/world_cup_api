f6127619f3b52d83ff3feb55f9afb2e2939e68bc
leejooho77 <leejooho@uga.edu>
2018-05-24 14:08:48 -0400

    Update stadium.sql

--- sql/stadium.sql
+++ sql/stadium.sql
@@ -1,16 +1,16 @@
 SET SQL_SAFE_UPDATES = 0;
 
-INSERT INTO `world_cup`.`stadium` (`name`, `city`, `image`) VALUES (`Luzhniki Stadium`, `Moscow`, `https://upload.wikimedia.org/wikipedia/commons/e/e6/Luzhniki_Stadium%2C_Moscow.jpg`);
-INSERT INTO `world_cup`.`stadium` (`name`, `city`, `image`) VALUES (`Ekaterinburg Arena`, `Ekaterinburg`, `https://upload.wikimedia.org/wikipedia/commons/d/d2/Estadio_Central_%28Ekaterinburg-arena%29.jpg`);
-INSERT INTO `world_cup`.`stadium` (`name`, `city`, `image`) VALUES (`Saint Petersburg Stadium`, `Saint Petersburg`, `https://upload.wikimedia.org/wikipedia/commons/a/ad/Spb_06-2017_img40_Krestovsky_Stadium.jpg`);
-INSERT INTO `world_cup`.`stadium` (`name`, `city`, `image`) VALUES (`Kaliningrad Stadium`, `Kaliningrad`, `https://upload.wikimedia.org/wikipedia/commons/a/a4/Kaliningrad_stadium_-_2018-04-07.jpg`);
-INSERT INTO `world_cup`.`stadium` (`name`, `city`, `image`) VALUES (`Kazan Arena`, `Kazan`, `https://upload.wikimedia.org/wikipedia/commons/3/3a/Kazan_Arena_08-2016.jpg`);
-INSERT INTO `world_cup`.`stadium` (`name`, `city`, `image`) VALUES (`Nizhny Novgorod Stadium`, `Nizhny Novgorod`, `https://upload.wikimedia.org/wikipedia/commons/9/90/Nizhny_Novgorod_Stadium_%28March_2018%29.jpg`);
-INSERT INTO `world_cup`.`stadium` (`name`, `city`, `image`) VALUES (`Samara Arena`, `Samara`, `http://img.fifa.com/image/upload/t_l4/d0mymt1ubl2pypmu3gn3.jpg`);
-INSERT INTO `world_cup`.`stadium` (`name`, `city`, `image`) VALUES (`Volgograd Arena`, `Volgograd`, `https://upload.wikimedia.org/wikipedia/commons/6/61/Construction_of_Volgograd_Arena_inside_04.jpg`);
-INSERT INTO `world_cup`.`stadium` (`name`, `city`, `image`) VALUES (`Mordovia Arena`, `Saransk`, `https://upload.wikimedia.org/wikipedia/commons/c/c9/%D0%A1%D1%82%D0%B0%D0%B4%D0%B8%D0%BE%D0%BD_Mordovia_arena.jpg`);
-INSERT INTO `world_cup`.`stadium` (`name`, `city`, `image`) VALUES (`Rostov Arena`, `Rostov-on-Don`, `https://upload.wikimedia.org/wikipedia/commons/9/94/Rostov-Arens_%28april_2018%29_01.jpg`);
-INSERT INTO `world_cup`.`stadium` (`name`, `city`, `image`) VALUES (`Fisht Stadium`, `Sochi`, `https://upload.wikimedia.org/wikipedia/commons/5/55/Fisht_Stadium_in_January_2018.jpg`);
-INSERT INTO `world_cup`.`stadium` (`name`, `city`, `image`) VALUES (`Spartak Stadium`, `Moscow`, `https://upload.wikimedia.org/wikipedia/commons/5/50/Stadium_Spartak_in_Moscow.jpg`);
+INSERT INTO `world_cup`.`stadium` (`name`, `city`, `image`) VALUES ('Luzhniki Stadium', 'Moscow', 'https://upload.wikimedia.org/wikipedia/commons/e/e6/Luzhniki_Stadium%2C_Moscow.jpg');
+INSERT INTO `world_cup`.`stadium` (`name`, `city`, `image`) VALUES ('Ekaterinburg Arena', 'Ekaterinburg', 'https://upload.wikimedia.org/wikipedia/commons/d/d2/Estadio_Central_%28Ekaterinburg-arena%29.jpg');
+INSERT INTO `world_cup`.`stadium` (`name`, `city`, `image`) VALUES ('Saint Petersburg Stadium', 'Saint Petersburg', 'https://upload.wikimedia.org/wikipedia/commons/a/ad/Spb_06-2017_img40_Krestovsky_Stadium.jpg');
+INSERT INTO `world_cup`.`stadium` (`name`, `city`, `image`) VALUES ('Kaliningrad Stadium', 'Kaliningrad', 'https://upload.wikimedia.org/wikipedia/commons/a/a4/Kaliningrad_stadium_-_2018-04-07.jpg');
+INSERT INTO `world_cup`.`stadium` (`name`, `city`, `image`) VALUES ('Kazan Arena', 'Kazan', 'https://upload.wikimedia.org/wikipedia/commons/3/3a/Kazan_Arena_08-2016.jpg');
+INSERT INTO `world_cup`.`stadium` (`name`, `city`, `image`) VALUES ('Nizhny Novgorod Stadium', 'Nizhny Novgorod', 'https://upload.wikimedia.org/wikipedia/commons/9/90/Nizhny_Novgorod_Stadium_%28March_2018%29.jpg');
+INSERT INTO `world_cup`.`stadium` (`name`, `city`, `image`) VALUES ('Samara Arena', 'Samara', 'http://img.fifa.com/image/upload/t_l4/d0mymt1ubl2pypmu3gn3.jpg');
+INSERT INTO `world_cup`.`stadium` (`name`, `city`, `image`) VALUES ('Volgograd Arena', 'Volgograd', 'https://upload.wikimedia.org/wikipedia/commons/6/61/Construction_of_Volgograd_Arena_inside_04.jpg');
+INSERT INTO `world_cup`.`stadium` (`name`, `city`, `image`) VALUES ('Mordovia Arena', 'Saransk', 'https://upload.wikimedia.org/wikipedia/commons/c/c9/%D0%A1%D1%82%D0%B0%D0%B4%D0%B8%D0%BE%D0%BD_Mordovia_arena.jpg');
+INSERT INTO `world_cup`.`stadium` (`name`, `city`, `image`) VALUES ('Rostov Arena', 'Rostov-on-Don', 'https://upload.wikimedia.org/wikipedia/commons/9/94/Rostov-Arens_%28april_2018%29_01.jpg');
+INSERT INTO `world_cup`.`stadium` (`name`, `city`, `image`) VALUES ('Fisht Stadium', 'Sochi', 'https://upload.wikimedia.org/wikipedia/commons/5/55/Fisht_Stadium_in_January_2018.jpg');
+INSERT INTO `world_cup`.`stadium` (`name`, `city`, `image`) VALUES ('Spartak Stadium', 'Moscow', 'https://upload.wikimedia.org/wikipedia/commons/5/50/Stadium_Spartak_in_Moscow.jpg');
 
 SET SQL_SAFE_UPDATES = 1;
