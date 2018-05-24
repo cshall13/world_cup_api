2b48475f39c820a64069bd27e040071e59ba961d
leejooho77 <leejooho@uga.edu>
2018-05-24 14:08:22 -0400

    Update group.sql

--- sql/group.sql
+++ sql/group.sql
@@ -1,13 +1,13 @@
 SET SQL_SAFE_UPDATES = 0;
 
-INSERT INTO `world_cup`.`group` (`name`) VALUES (`Group A`);
-INSERT INTO `world_cup`.`group` (`name`) VALUES (`Group B`);
-INSERT INTO `world_cup`.`group` (`name`) VALUES (`Group C`);
-INSERT INTO `world_cup`.`group` (`name`) VALUES (`Group D`);
-INSERT INTO `world_cup`.`group` (`name`) VALUES (`Group E`);
-INSERT INTO `world_cup`.`group` (`name`) VALUES (`Group F`);
-INSERT INTO `world_cup`.`group` (`name`) VALUES (`Group G`);
-INSERT INTO `world_cup`.`group` (`name`) VALUES (`Group H`);
-INSERT INTO `world_cup`.`group` (`name`) VALUES (`null`);
+INSERT INTO `world_cup`.`group` (`name`) VALUES ('Group A');
+INSERT INTO `world_cup`.`group` (`name`) VALUES ('Group B');
+INSERT INTO `world_cup`.`group` (`name`) VALUES ('Group C');
+INSERT INTO `world_cup`.`group` (`name`) VALUES ('Group D');
+INSERT INTO `world_cup`.`group` (`name`) VALUES ('Group E');
+INSERT INTO `world_cup`.`group` (`name`) VALUES ('Group F');
+INSERT INTO `world_cup`.`group` (`name`) VALUES ('Group G');
+INSERT INTO `world_cup`.`group` (`name`) VALUES ('Group H');
+INSERT INTO `world_cup`.`group` (`name`) VALUES ('null');
 
 SET SQL_SAFE_UPDATES = 1;
